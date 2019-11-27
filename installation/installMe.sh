53b7d75bc4326da957e9f1d73be84be9b728a910
vakanto <34423633+vakanto@users.noreply.github.com>
2019-11-27 17:40:41 +0100

    Update installMe.sh

--- installation/installMe.sh
+++ installation/installMe.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 chmod +x gui-docker
-sudo apt install unzip
 git clone https://github.com/Kinovarobotics/kinova-ros.git
 docker pull nvidia/cuda
 docker pull moveit/moveit:melodic-release

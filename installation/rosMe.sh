693534811a15a2863f09a16932893201e78b6e55
vakanto <vakanto@posteo.eu>
2020-01-02 18:02:06 +0100

    Added demo command to launch script

--- installation/rosMe.sh
+++ installation/rosMe.sh
@@ -2,12 +2,13 @@
 apt update
 apt install libblas-dev liblapack-doc liblapack-dev bash-completion vim
 
-source /opt/ros/melodic/setup.bash
-source /opt/ros/melodic/devel/setup.bash
 mkdir /opt/ros/melodic/src
 (
 cd /opt/ros/melodic
 catkin_make
 )
+source /opt/ros/melodic/setup.bash
+source /opt/ros/melodic/devel/setup.bash
 #roscore &
 #rosrun rviz rviz
+#roslaunch j2n6s300_moveit_config j2n6s300_virtual_robot_demo.launch

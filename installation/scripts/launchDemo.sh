d0e41a1653b00403865e4c0b28668b7c84a4bf9e
vakanto <vakanto@posteo.eu>
2020-01-09 20:33:12 +0100

    Changed environment

--- installation/scripts/launchDemo.sh
+++ installation/scripts/launchDemo.sh
@@ -4,4 +4,4 @@ source /opt/ros/melodic/devel/setup.bash
 
 roslaunch j2n6s300_moveit_config j2n6s300_virtual_robot_demo.launch &
 
-rosrun kinova_arm_moveit_demo pick_place 
+rosrun kinova_arm_moveit_demo work_scene

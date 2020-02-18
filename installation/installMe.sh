810f6611d0c04cc43a5b2b169df303f978478fdd
vakanto <vakanto@posteo.eu>
2020-02-18 20:40:16 +0100

    Fixed Demo

--- installation/installMe.sh
+++ installation/installMe.sh
@@ -2,6 +2,9 @@
 
 chmod +x gui-docker
 test -d ./kinova-ros || git clone https://github.com/Kinovarobotics/kinova-ros.git
-docker pull nvidia/cuda
-docker pull moveit/moveit:melodic-release
+test -d ./moveit_tutorials || git clone https://github.com/ros-planning/moveit_tutorials.git -b melodic-devel
+test -d ./panda_moveit_config || git clone https://github.com/ros-planning/panda_moveit_config.git -b melodic-devel
+#docker pull nvidia/cuda
+#docker pull moveit/moveit:melodic-release
 docker pull ros:latest
+docker build -t teamarbeit/rosme .
--- installation/launchMe.sh
+++ installation/launchMe.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-./gui-docker -it --rm moveit/moveit:melodic-release /bin/bash
+#./gui-docker -it --rm moveit/moveit:melodic-release /bin/bash
+./gui-docker -it --rm teamarbeit/rosme /bin/bash

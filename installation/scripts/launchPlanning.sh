efa83e348c1edbcefa90a47b727b3a3487a6d15c
vakanto <vakanto@posteo.eu>
2020-02-18 20:54:02 +0100

    Fix Demo 2.0

--- installation/scripts/launchPlanning.sh
+++ installation/scripts/launchPlanning.sh
@@ -3,8 +3,6 @@ source /opt/ros/melodic/setup.bash
 source /opt/ros/melodic/devel/setup.bash
 
 #roslaunch j2n6s300_moveit_config j2n6s300_virtual_robot_demo.launch &
-roslaunch panda_moveit_config demo.launch &
-sleep 5
 roslaunch moveit_tutorials move_group_interface_tutorial.launch
 
 #rosrun kinova_arm_moveit_demo work_scene

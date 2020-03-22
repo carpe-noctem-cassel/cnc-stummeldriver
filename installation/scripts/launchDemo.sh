241f904d0ca3c365ef325e6d3218c939b807b9f1
vakanto <vakanto@posteo.eu>
2020-03-22 12:56:02 +0100

    Added setup agent, added j2n6s300 model

--- installation/scripts/launchDemo.sh
+++ installation/scripts/launchDemo.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 source /opt/ros/melodic/setup.bash
 source /opt/ros/melodic/devel/setup.bash
-
+roslaunch moveit_setup_assistant setup_assistant.launch
 #roslaunch j2n6s300_moveit_config j2n6s300_virtual_robot_demo.launch &
-roslaunch panda_moveit_config demo.launch &
-sleep 5
-roslaunch moveit_tutorials move_group_interface_tutorial.launch
+#roslaunch panda_moveit_config demo.launch &
+#sleep 5
+#roslaunch moveit_tutorials move_group_interface_tutorial.launch
 
 #rosrun kinova_arm_moveit_demo work_scene
 #rosrun kinova_arm_moveit_demo pick_place.cpp
